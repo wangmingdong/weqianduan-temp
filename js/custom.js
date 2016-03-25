ce0d28d8baa065124249e8ed05e5beb354df5663
wangmingdong <wangmingdong@navinfo.com>
2016-03-25 20:50:27 +0800

    联系模块样式更改

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,8 @@
-/* Theme Name: Worthy - Free Powerful Theme by HtmlCoder
- * Author:HtmlCoder
- * Author URI:http://www.htmlcoder.me
- * Version:1.0.0
- * Created:November 2014
- * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
- * File Description: Place here your custom scripts
- */
+$(function(){
+	$(".social-links li.weixin").mouseenter(function(){
+		$(".wechat").show();
+	});
+	$(".remove-wechat").click(function(){
+		$(".wechat").hide();
+	})
+});
